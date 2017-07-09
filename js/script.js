ea8d28b19668da8a2ad735b86ee87e8a2fcf4542
Raju Jha <rajujha373@gmail.com>
2017-07-09 10:18:28 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$("#notavailable").click(function(event) {
+		alert("Sorry, this project has not been hosted on any site! Please check out the source code on GitHub Instead :)")
+	});
+});
+
+
+
+
